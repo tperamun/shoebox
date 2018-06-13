e8f2e5e0f67a8ece824fe436c4f3f6afe2b4a46b
root <root@localhost.localdomain>
2018-06-12 22:18:22 -0400

    ajax finally working, make some UI changes

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('login-signup.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+	print("hellloooo")
+	email = request.form['email']
+	name = request.form['name']
+	
+	
+	if name and email:
+		newName = name[::-1]
+
+		return jsonify({'name' : newName})
+
+	return jsonify({'error' : 'Missing data!'})
+
+if __name__ == '__main__':
+	app.run(debug=True)
